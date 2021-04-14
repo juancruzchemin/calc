fdce80f251b62474dec098c9fe7948585693c7a7
manuel-robles <manuu.robles@gmail.com>
2021-04-14 16:09:19 -0300

    comiteamos la funcion div

--- fns.js
+++ fns.js
@@ -23,6 +23,15 @@ function mul(a, b) {
 function div(a, b) {
     // Tener en cuenta division por 0
     // Si b es 0 retornar "Error: div by 0"
+    if(b == 0){
+        return "error: div by 0";
+    }
+    else{
+        return a/b;
+    }    
+
+
+
 }
      
 function pow(a, b) {}
